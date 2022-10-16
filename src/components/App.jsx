ae862ddc3821761a9cbda4c162ce98617c30b5e4
OlgaMykhailova <rosebud_z@yahoo.com>
2022-10-16 11:23:39 +0200

    added saving to local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,6 +15,20 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = newContact => {
     const contactList = this.state.contacts;
     const newContactNameNormalized = newContact.name.toLowerCase();
